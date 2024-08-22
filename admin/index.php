3624fe322e4305fa003cac9bfdea84f5e5b12d66
Remzloi <110280681+Remzloi@users.noreply.github.com>
2024-08-22 15:00:47 +0400

    Create index.php

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+	include "../config.php"
+?>
+<html>
+	<head>
+		<title>Admin Panel - <?php echo "$instancename"?></title>
+	</head>
+	<body>
+		<h1><?php echo "$instancename"?> Admin Panel</h1>
+	</body>
+</html>
