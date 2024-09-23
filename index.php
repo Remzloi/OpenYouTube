6b3d617f0cf2efa9bca373555d55eeff449d3deb
Remzloi <110280681+Remzloi@users.noreply.github.com>
2024-09-23 18:31:09 +0400

    Add new configs, version и т.д я не знаю английский извините

--- index.php
+++ index.php
@@ -3,6 +3,26 @@
 ?>
 <html>
 	<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
 		<title><?php echo "$instancename"?></title>
 	</head>
+	<body>
+		<div id="yt-masthead-container" class="yt-grid-box">
+			<div id="yt-masthead">
+				<a href="index.php" id="logo-container" alt="<?php echo "$instancename"?> home">
+					<img id="logo" src="<?php echo "$instancelogo"?>" alt="<?php echo "$instancename"?> home">
+				</a>
+				<div id="yt-masthead-signin">
+					<button type="button" class=" yt-uix-button yt-uix-button-primary" onclick=";window.location.href=this.getAttribute('href');return false;" href="https://example.com" role="button">
+						<span class="yt-uix-button-content">Sign in 
+  						</span>
+					</button>
+				</div>
+			</div>
+		</div>
+		<div id="footer-container">
+			<p>OpenYouTube Pre-Alpha by RemZloev</p>
+			<p>Instance by <?php echo "$instanceadmin"?></p>
+		</div>
+	</body>
 </html>
